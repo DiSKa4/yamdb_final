78ca9711c10c25c05d6cceee10d005c1443b12c8
DiSKa4 <96827287+DiSKa4@users.noreply.github.com>
2022-08-06 18:04:24 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,11 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '84.201.162.42'
-    'localhost',
-    '*'
-]
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
