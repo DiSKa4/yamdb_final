3bb3c92d835145c60486b15fa73df387619a72b5
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-08-06 17:43:39 +0300

    commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,11 +3,15 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '84.201.162.42'
+    'localhost',
+    '*'
+]
 
 
 INSTALLED_APPS = [
