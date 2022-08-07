87becadab72ca1571514c29cd6d07627f282b34d
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-08-07 13:32:07 +0300

    commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS =['84.201.162.42', '127.0.0.1', 'localhost'] 
+ALLOWED_HOSTS = ['84.201.162.42', '127.0.0.1', 'localhost']
 
 
 INSTALLED_APPS = [
